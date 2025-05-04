e10b4af84175af76d5d4eabcb1960bcf1e2376ef
Collin Wischmcj <cjwischmeyer@gmail.com>
2025-05-04 18:49:46 -0400

    moving scripts for starting containers, pulling logs, etc to their own directory

--- loop_and_log.sh
+++ loop_and_log.sh
@@ -6,7 +6,11 @@ for i in {0..19}; do
   output="$(sudo docker logs -t archiveteam_$i)"
   touch $new_file
   echo $output >> $new_file
-  echo $output >> $agg_file
+  # echo $output >> $agg_file
+  grep -E "sent \b[0-9]{1,3}(,[0-9]{3})*(\.[0-9]+)?\b bytes" -o $new_file
   echo "wrote logs for ${i} to ${new_file}"
 done
+# grep -E "[0-9]{4}-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9]{9}Z sent \b[0-9]{1,3}(,[0-9]{3})*(\.[0-9]+)?\b bytes " -o archiveteam_agg_20250307.log >> /sent.log
+# grep -E "[0-9]{4}-[0-9][0-9]-[0-9][0-9]T[0-9][0-9]:[0-9][0-9]:[0-9][0-9].[0-9]{9}Z sent \b[0-9]{1,3}(,[0-9]{3})*(\.[0-9]+)?\b bytes " -o archiveteam_agg_20250307.log >> /sent.log
+
 exit 0
